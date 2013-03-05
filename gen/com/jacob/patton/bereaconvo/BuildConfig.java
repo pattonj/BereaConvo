4caba1f4c2ee36fbbe8772ec75191a15de90f092
Jacob Patton <pattonj@berea.edu>
2013-03-05 14:37:34 -0500

    First App commit
    A layout using the two libraries.

---
+++ gen/com/jacob/patton/bereaconvo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jacob.patton.bereaconvo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/jacob/patton/bereaconvo/MenuBarMainActivity.java
@@ -0,0 +1,49 @@
+package com.jacob.patton.bereaconvo;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+//import com.actionbarsherlock.app.SherlockActivity;
+
+
+
+import android.os.Bundle;
+//import android.support.v4.app.FragmentTransaction;
+//import android.widget.TextView;
+
+
+
+public class MenuBarMainActivity extends SherlockActivity {
+	 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+        menu.add("Fall")
+        	.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+        menu.add("Spring")
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        
+        menu.add("Settings")
+        .setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+        
+        menu.add("About")
+        .setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+        
+        return true;
+    }
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getSupportActionBar().setDisplayShowHomeEnabled(false);
+	}
+
+	
+	
+    	
+    	
+    
+
+}
