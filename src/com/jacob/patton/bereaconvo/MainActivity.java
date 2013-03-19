fbd60572ec21894c62bf31c06fcb123750d0eff3
Jacob Patton <pattonj@berea.edu>
2013-03-19 12:38:58 -0400

    Array Database code
    Added the full 2 dimensional array database and fall/spring arrays.

--- src/com/jacob/patton/bereaconvo/MainActivity.java
+++ src/com/jacob/patton/bereaconvo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jacob.patton.bereaconvo;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -15,7 +17,9 @@ import com.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends SherlockFragmentActivity {
 	
-	
+	public ArrayList<String[]> database;
+	public ArrayList<String[]> dbFall;
+	public ArrayList<String[]> dbSpring;
 	 
 	public boolean onCreateOptionsMenu(Menu menu) {
 		
@@ -56,11 +60,59 @@ public class MainActivity extends SherlockFragmentActivity {
 	        
 	    }	
 
+	
+	public void createMaster(){
+		// creates database list 
+		// Currently this is random, later this will be where the parse goes. 
+		// This is used instead of two separate semester lists as to make parsing data easier down the road.
+		// The array is as follows [semester, date,time, description]
+		 database = new ArrayList<String[]>();
+			for(int i=0; i<5; i++){
+				database.add(new String[]{"Fall","i/8/12","3:00pm","This is a description of the data that will be used"});
+				database.add(new String[]{"Spring","i/2/13","3:00pm","This is a description of the data that will be used"});
+			}
+			for(int i=0; i<2; i++){
+				database.add(new String[]{"Fall","i/2/13","8:00pm","This is a description of the data that will be used"});
+				database.add(new String[]{"Spring","i/2/14","8:00pm","This is a description of the data that will be used"});
+			}
+			for(int i=0; i<2; i++){
+				database.add(new String[]{"Fall","i/2/13","6:00pm","This is a description of the data that will be used"});
+				database.add(new String[]{"Spring","i/2/14","6:00pm","This is a description of the data that will be used"});
+			}
+	}
+	
+	
+	
+	public void createSemesters(){
+		// Both semester databases have been declared at the start, so initialize them here.  
+		dbFall = new ArrayList<String[]>();
+		dbSpring = new ArrayList<String[]>();
+		
+		// loop it the number of times for the size of the database. 
+		for(int i = 0; i< database.size(); i++){
+			// if the first array value is fall, add to fall database. 
+			if(database.get(i)[0].equals("Fall")){
+				dbFall.add(database.get(i));
+			}
+			// Check to see if it's spring, and add it to spring. 
+			else if(database.get(i)[0].equals("Spring")){
+				dbSpring.add(database.get(i));
+			}
+			else{
+				// it's misspelled or an error so ignore. 
+			}
+			
+		}
+		
+	}
+	
+	
 	/*
 	 * EXAMPLE SUBROUTINES
 	 * 
-	 * Determine if Master arraylists exist. 
+	 * Determine if Master arraylists exist. -- added  
 	 * 		If not (create master list){
+	 * 			
 	 *         	inside master list(){
 	 * 				Semester
 	 * 				Date				
@@ -71,7 +123,7 @@ public class MainActivity extends SherlockFragmentActivity {
 	 * 	
 	 * 
 	 * 
-	 * Create Semester Array list (int semester){
+	 * Create Semester Array list (int semester){ -- added. 
 	 * 			if 1 = fall
 	 * 			if 2 = spring
 	 * 		create a copy of master with only ones that are fall or spring
