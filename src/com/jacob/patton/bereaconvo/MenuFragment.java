8be0fdb6ce9a2765ff6669e4dabc94bd8445f912
Jacob Patton <pattonj@berea.edu>
2013-03-21 09:31:48 -0400

    Fragments update
    Added button to side fragment to update random text, changed fragment
    and layout names to make it more clear.

---
+++ src/com/jacob/patton/bereaconvo/MenuFragment.java
@@ -0,0 +1,49 @@
+package com.jacob.patton.bereaconvo;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MenuFragment extends SherlockFragment{
+	TextView text;
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		
+	super.onCreate(savedInstanceState);
+	// set content view
+	
+	// this sets our layout as a variable. 
+	 LinearLayout myLayout= (LinearLayout) inflater.inflate(R.layout.fragment_menu, container, false);
+	 // then we can access the button inside the layout. 
+	 Button mButton = (Button) myLayout.findViewById(R.id.SideButton);
+	
+	 	mButton.setOnClickListener(new OnClickListener() {
+	        @Override
+	        public void onClick(View v) {
+	            update();
+	        }
+	 	});
+
+
+
+	 return myLayout;
+	 
+		 
+}
+
+// used to update the fragment text. 
+public void update(){
+	text = (TextView) getView().findViewById(R.id.TextBoxMenu);
+	text.setText("working");
+
+}
+	
+
+}
